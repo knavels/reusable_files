72dbab15a8867283b7f53e244db2bf380204b651
Navid Dezashibi <dezashibi@gmail.com>
2023-08-18 11:17:34 +0400

    Create main.cpp

---
+++ new_cpp_project/src/main.cpp
@@ -0,0 +1,20 @@
+/***************************************************************************************
+    File: main.cpp
+    Author: Navid Dezashibi
+    Initial Creation Date: d/mm/yyyy
+    Contact: navid@dezashibi.com
+    Website: https://dezashibi.com
+    License:
+    Please refer to the LICENSE file, repository or website for more information about
+    the licensing of this work. If you have any questions or concerns,
+    please feel free to contact me at the email address provided above.
+ **************************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Welcome!" << std::endl;
+
+	return 0;
+}
